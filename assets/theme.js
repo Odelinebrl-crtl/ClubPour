619f1c193ec199fdc5c15f6834030c45b10279fb
Odelinebrl-crtl <odeline.brouiller@gmail.com>
2026-08-20 00:15:15 +0200

    Update theme.js

--- assets/theme.js
+++ assets/theme.js
@@ -1,1 +1,19 @@
 document.documentElement.classList.add('js');
+
+const header = document.querySelector('.site-header');
+
+if (header) {
+  const updateHeader = () => {
+    if (window.scrollY > 40) {
+      header.classList.add('is-scrolled');
+    } else {
+      header.classList.remove('is-scrolled');
+    }
+  };
+
+  updateHeader();
+
+  window.addEventListener('scroll', updateHeader, {
+    passive: true
+  });
+}
