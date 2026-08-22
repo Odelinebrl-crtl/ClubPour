bc4b68469a840b2bac8311512dcf6b1ed8b8734b
Odelinebrl-crtl <odeline.brouiller@gmail.com>
2026-08-22 19:05:19 +0200

    Update theme.js

--- assets/theme.js
+++ assets/theme.js
@@ -76,7 +76,7 @@ function initPourQuoteReveal() {
       */
       if (
         scrollingDown &&
-        rect.top < viewportHeight * 0.75 &&
+        rect.top < viewportHeight * 0.72 &&
         rect.bottom > viewportHeight * 0.07
       ) {
         quote.classList.add('is-visible');
